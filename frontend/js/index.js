560f3d19f3a18cb87469cd913bb5dea4fadadce9
GimpFlamingo <rayjkochenderfer@gmail.com>
2019-11-29 23:50:47 -0500

    Added more sections

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,18 +1,28 @@
 window.onload = () => {
-    // Opens the start-game section to host a local game
-    document.getElementById('start-game').onclick = () => {
-
-    }
-    // Opens the join-game menu to join a local game
-    document.getElementById('join-game').onclick = () => {
-        
-    }
-    // Opens the handbook reference section
-    document.getElementById('handbook').onclick = () => {
-        
-    }
-    // Opens the section to the user's characters and create a character
-    document.getElementById('characters').onclick = () => {
-        
-    }
+	var homeSection = document.getElementById('home')
+	var gmSection = document.getElementById('gm-screen')
+	var characterSection = document.getElementById('character-sheet')
+	var handbookSection = document.getElementById('handbook')
+	var joinSection = document.getElementById('join-game')
+	var startSection = document.getElementById('start-game')
+	// Opens the start-game section to host a local game
+	document.getElementById('start-game').onclick = () => {
+		homeSection.style.display = 'none'
+        startSection.style.display = 'block'
+	}
+	// Opens the join-game menu to join a local game
+	document.getElementById('join-game').onclick = () => {
+		homeSection.style.display = 'none'
+        joinSection.style.display = 'block'
+	}
+	// Opens the handbook reference section
+	document.getElementById('handbook').onclick = () => {
+		homeSection.style.display = 'none'
+        handbookSection.style.display = 'block'
+	}
+	// Opens the section to the user's characters and create a character
+	document.getElementById('characters').onclick = () => {
+		homeSection.style.display = 'none'
+		characterSection.style.display = 'block'
+	}
 }
