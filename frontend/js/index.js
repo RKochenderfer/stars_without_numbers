bf3827a2af6fb039dc79a161acdf939389626142
GimpFlamingo <rayjkochenderfer@gmail.com>
2019-11-29 23:38:19 -0500

    Added listners

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,18 @@
 window.onload = () => {
-    
+    // Opens the start-game section to host a local game
+    document.getElementById('start-game').onclick = () => {
+
+    }
+    // Opens the join-game menu to join a local game
+    document.getElementById('join-game').onclick = () => {
+        
+    }
+    // Opens the handbook reference section
+    document.getElementById('handbook').onclick = () => {
+        
+    }
+    // Opens the section to the user's characters and create a character
+    document.getElementById('characters').onclick = () => {
+        
+    }
 }
